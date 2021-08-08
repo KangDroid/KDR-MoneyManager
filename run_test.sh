5f596e548236e89d240364b0267a1c1a31811c9d
KangDroid <hyunwoo286@gmail.com>
2021-08-08 19:31:12 +0900

    Test Script: Proper test

--- run_test.sh
+++ run_test.sh
@@ -2,7 +2,7 @@
 
 # Run Test first[Threshold: 90% or more] - Use %2c instead of comma
 # See https://github.com/coverlet-coverage/coverlet/issues/182
-dotnet test -p:CoverletOutputFormat=cobertura -p:CollectCoverage=true -p:Threshold=90  #-p:ExcludeByFile="**/Migrations/*.cs%2c**/Model/**/*.cs"
+dotnet test -p:CoverletOutputFormat=cobertura -p:CollectCoverage=true -p:Threshold=90  -p:ExcludeByFile="**/Migrations/*.cs%2c**/Model/**/*.cs"
 
 # now in root folder, file name called coverage.cobertura.xml should exists.
 reportgenerator -reports:"MoneyManagerTest/coverage.cobertura.xml" -targetdir:"coveragereport" -reporttypes:Html
